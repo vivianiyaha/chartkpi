ff9ffeb33594298d61bcec71cc75fd09af82d278
Vivian Iyaha <90201480+vivianiyaha@users.noreply.github.com>
2026-06-03 23:43:21 +0100

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="HR/Admin Management System",
+    page_icon="logo.png",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
 # =========================================
 # APPLICATION HUB
 # =========================================
